ed654e092aaa9a00274f78de3e2afb3d3cc5a1e4
Mauro <dantuchiii@gmail.com>
2024-03-23 17:30:27 -0300

    Arreglado error de variables de entorno

--- client/src/helpers/getEnvVariables.ts
+++ client/src/helpers/getEnvVariables.ts
@@ -2,10 +2,11 @@
 
 export const getEnvVariables = () => {
 
-    import.meta.env
+    // import.meta.env
 
     return {
-        ...import.meta.env
+        // ...import.meta.env
+        VITE_API_URL: import.meta.env.VITE_API_URL
     }
 
 }
